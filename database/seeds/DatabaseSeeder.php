2f0141907e9c18dcaee900e1411bd135c4df13ff
Mladen Jelovac <jelovacmladen@gmail.com>
2018-09-22 17:50:05 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /* $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
+         $this->call(UsersTableSeeder::class);
+     /*   DB::table('users')->insert([
             
         ]); */
     }
