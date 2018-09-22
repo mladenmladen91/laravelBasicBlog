9b448c87786f2ef0037412605dd14c2ffc501a52
vasko <vjelovac90@gmail.com>
2018-09-22 17:53:23 +0200

    updating seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /* $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
-            
-        ]); */
+         $this->call(UsersTableSeeder::class);
+    
     }
 }
